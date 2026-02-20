16a68569a61b1c4c58e99585bafff2ebc24d7290
srikanth <csri@nuuday.com>
2026-02-20 11:27:29 +0530

    added ec2.tf

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,1 +1,30 @@
-#aws ec2 instance
+resource "aws_instance" "example" {
+  
+  ami= "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+  tags = {
+    Name = "frontend"
+  }
+}
+
+
+resource "aws_security_group" "allow_all" {
+    name= "allow_all"
+    description = "allow_allports"
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+  
+}
