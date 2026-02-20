086a190266e30f8e723c4338950535ff7ccab4cf
srikanth <csri@nuuday.com>
2026-02-20 13:37:49 +0530

    updated count.index value

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,10 +1,12 @@
 resource "aws_instance" "example" {
-  
+#  count = 2 
   ami= "ami-0220d79f3f480ecf5"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all.id]
   tags = {
     Name = "frontend"
+#    Name = "frontend-${count.index}"
+# if you define as above ,     here  count.index iterate based on count value it creates name of the instances as frontend-o and frontend-1 
   }
 }
 
