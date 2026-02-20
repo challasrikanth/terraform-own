fefa5678ad3ae3a7012c17e50070a422d48a711d
srikanth <csri@nuuday.com>
2026-02-20 11:49:40 +0530

    added variable related files

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,1 +1,31 @@
 
+resource "aws_instance" "var1" {
+
+    ami = var.ami_id
+    instance_type = var.instance_type
+
+  tags = var.ec2_tags
+}
+
+
+resource "aws_security_group" "allow_all" {
+
+    name = var.sg_name
+    description = var.sg_description
+
+    egress = {
+        from_port = var.sg_from_port
+        to_port = var.sg_to_port
+        protocol = "-1"
+        cidr_blocks = var.cidr_blocks
+    }
+
+    ingress = {
+        from_port = var.sg_from_port
+        to_port = var.sg_to_port
+        protocol = "-1"
+        cidr_blocks = var.cidr_blocks
+    }
+    
+    tags = var.sg_tags
+}
