f6c5e4311a531caa79e2a95ad0391ee55f36648e
srikanth <csri@nuuday.com>
2026-02-20 12:03:38 +0530

    added .gitignore file

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -3,8 +3,9 @@ resource "aws_instance" "var1" {
 
     ami = var.ami_id
     instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.allow_all.id]
 
-  tags = var.ec2_tags
+    tags = var.ec2_tags
 }
 
 
@@ -13,14 +14,14 @@ resource "aws_security_group" "allow_all" {
     name = var.sg_name
     description = var.sg_description
 
-    egress = {
+    egress {
         from_port = var.sg_from_port
         to_port = var.sg_to_port
         protocol = "-1"
         cidr_blocks = var.cidr_blocks
     }
 
-    ingress = {
+    ingress {
         from_port = var.sg_from_port
         to_port = var.sg_to_port
         protocol = "-1"
